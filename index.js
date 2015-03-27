98eba47ccb4f63088b3647d0a72b730d36897c49
Trent Willis <trentmwillis@gmail.com>
2015-03-26 20:57:34 -0700

    Bunch of minor fixes. Bump ember-cli version.

--- index.js
+++ index.js
@@ -3,22 +3,24 @@
 
 var fs = require('fs');
 var path = require('path');
-var stylesDir;
 
 /**
  * Recursive method to walk a directory and return a list of all it's files
+ * with their paths relative to either a given directory or the directory
+ * being walked
  * @param {String} dir - A string representing the directory to walk
  * @param {String} relDir - A directory to make the results relative to
  * @return {Array} results
  */
-var walkDir = function(dir, relDir) {
-  var list = fs.readdirSync(dir);
-  var relDir = relDir || dir;
+function walkDir(dir, relDir) {
+  var fileList = fs.readdirSync(dir);
   var results = [];
-  var stat;
+  var stat, file;
 
-  list.forEach(function(file) {
-    file = path.resolve(dir, file);
+  relDir = relDir || dir;
+
+  for (var i=0, l=fileList.length; i<l; i++) {
+    file = path.resolve(dir, fileList[i]);
     stat = fs.statSync(file);
 
     if (stat && stat.isDirectory()) {
@@ -26,27 +28,33 @@ var walkDir = function(dir, relDir) {
     } else {
       results.push(path.relative(relDir, file));
     }
-  });
+  }
 
   return results;
 }
 
 module.exports = {
   name: 'ember-css-routes',
+
+  /**
+   * @override
+   * Creates a mapping of route stylesheets from the app/styles directory
+   * to the assets directory.
+   * @return {Void}
+   */
   included: function() {
-    var styleFiles, styleMappings;
-    stylesDir = path.join(this.app.project.root, 'app/styles');
+    var stylesDir = path.resolve(this.project.configPath(), '../../app/styles');
+    var styleFiles = walkDir(stylesDir);
+    var styleMappings = {};
+    var filePath;
 
-    styleFiles = walkDir(stylesDir);
-    styleMappings = {};
+    for (var i=0, l=styleFiles.length; i<l; i++) {
+      filePath = styleFiles[i].split('.')[0];
 
-    styleFiles.forEach(function(file) {
-      var fileRoot = file.split('.')[0];
-
-      if (fileRoot.indexOf('styles') !== -1) {
-        styleMappings[fileRoot] = path.join('assets', fileRoot + '.css');
+      if (filePath.indexOf('styles') !== -1) {
+        styleMappings[filePath] = path.join('assets', filePath + '.css');
       }
-    });
+    };
 
     // Explicitly add the 'app' output, so it is named properly
     styleMappings['app'] = path.join('assets', this.app.name + '.css');
